5d9c44c814940c72d9f412c6a872c885858c7085
Craig Jolley <jolleycraig@gmail.com>
2020-01-03 16:01:06 -0500

    Added help text

--- server.R
+++ server.R
@@ -32,6 +32,33 @@ shinyServer(function(input, output, session) {
     plotOutput('dotPlot',width='auto',height=heightstr)
   })
   
+  output$how_to <- renderText({
+    str <- paste0("<b>How to read this plot:</b><br><br>",
+                  "<b><i><font color='#BA0C2F'>Red circles</font></i></b>: indicator values for ",input$country,'.<br>',
+                  "<b><i><font color='#002F6C'>Translucent blue circles</font></i></b>: indicator values for all other countries.<br>",
+                  "<b><i><span style='background-color: #CFCDC9'>Gray shaded region</i></b></span>: indicates the middle ",input$shade*100,"% of countries.<br><br>")
+    if (input$score) {
+      str <- paste0(str,
+                    "<b><i><font color='#BA0C2F'>Vertical red line</font></i></b>: Overall score for ",input$country,
+                    " based on the data shown in this plot. The overall score is the ",
+                    "first principal component (PC) of the data shown on this plot (with missing values for ",
+                    "countries other than ",input$country," filled in by multiple imputation).<br><br>")
+    }
+    if (input$pred) {
+      str <- paste0(str,
+                    "<b><i><font color='#BA0C2F'>Short vertical red lines</font></i></b>: Predicted value of indicator based on linear model of first ",
+                    input$pcs," principal components.<br><br>",
+                    "Red circles are <b><i><font color='#BA0C2F'>filled</font></i></b> if the difference between the model prediction and the actual ",
+                    "value is statistically-significant (p < 0.05). This can be taken as a sign that the ",
+                    'actual value is "surprising", given other data about the country.<br><br>',
+                    "Red circles are <b><i><font color='#BA0C2F'>hollow</font></i></b> if the difference is not statistically-significant. This sometimes ",
+                    "happens even for relatively large errors; this is just a sign that the indicator cannot be modeled ",
+                    "well based on the first ",input$pcs," principal components.<br><br>")
+    }
+    if (input$help_me) { str }
+  })
+  
+  
   # filter drop-down options as explained in this blog post:
   # https://www.davidsolito.com/post/conditional-drop-down-in-shiny/
   country_choice <- reactive({
--- ui.R
+++ ui.R
@@ -1,10 +1,10 @@
 # Development TODOs for DECA shiny app
 
-# make a utility file to re-generate plot_data.csv from scratch
-
 # Single scatterplot with multiple selectable countries
 # Add mouse-over to view names of other countries?
 # Add ability to select from a list of standard plots
+
+# re-calculate PCs; add to available plots
 
 # Design workflow that allows people to quickly generate all plots for desk review
 
@@ -14,7 +14,11 @@ library(shiny)
 
 shinyUI(fluidPage(
   
-  titlePanel("J2SR-style test plot"),
+  tags$head(
+    tags$style(HTML("hr {border-top: 1px solid #CFCDC9;}"))
+  ),
+  
+  titlePanel("DECA app (alpha)"),
   
   sidebarLayout(
     sidebarPanel(
@@ -23,7 +27,10 @@ shinyUI(fluidPage(
       checkboxInput('score','Show summary score?',TRUE),
       sliderInput('shade','Fraction to shade:',0,1,0.5),
       checkboxInput('pred','Show predictions?',FALSE),
-      uiOutput('pc_choice')
+      uiOutput('pc_choice'),
+      hr(),
+      checkboxInput('help_me','Show help text?',FALSE),
+      htmlOutput('how_to')
     ),
     
     mainPanel(
